680290f6120f8029a06556c70dd266f92d59b393
myname21-meet <myname21@meet.mit.edu>
2019-11-07 11:50:35 -0500

    Flasks

--- app.py
+++ app.py
@@ -5,8 +5,16 @@ app = Flask(__name__)
 app.secret_key = "MY_SUPER_SECRET_KEY"
 
 @app.route('/')
-def hello():
+def home():
 	return render_template("home.html")
+
+@app.route('/store')
+def store():
+	return render_template("store.html")
+
+@app.route('/about')
+def about():
+	return render_template("about.html")
 
 
 if __name__ == '__main__':
